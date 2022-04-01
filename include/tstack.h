2820f12bd432f7329ff0839a2f9b191b47bc079c
VilenskayaAlina <99177250+VilenskayaAlina@users.noreply.github.com>
2022-04-01 14:12:09 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,9 +1,41 @@
 // Copyright 2021 NNTU-CS
+#include <string>
+#include <map>
+#include "tstack.h"
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
-
+private:
+  T arr[50];
+  int top;
+public:
+  TStack() : top(-1) {}
+  bool isEmpty() {
+    return top == -1;
+  }
+  bool isFull() {
+    return top == size - 1;
+  }
+  void push(const T&value) {
+    if (!isFull())
+      arr[++top] = value;
+    else
+      throw "Full!";
+  }
+  T get() {
+    if (!isEmpty())
+      return arr[top];
+    else
+      throw "Empty!";
+  }
+  void pop() {
+    if (!isEmpty())
+      --top;
+    else
+      throw "Empty!";
+  }
 };
-#endif  // INCLUDE_TSTACK_H_
+#endif
+// INCLUDE_TSTACK_H_
